f7638bbfab345e8dc1ed70c76ac2118f476c5151
MarcOreliOoo <albeaurelien@gmail.com>
2023-09-07 15:05:05 +0200

    upd create hero api

---
+++ backend/src/Features/hero/dto/get-hero.dto.ts
@@ -0,0 +1,3 @@
+export class GetHeroDto {
+    name: string;
+}
--- backend/src/Features/hero/hero.controller.ts
+++ backend/src/Features/hero/hero.controller.ts
@@ -1,16 +1,25 @@
 import { Controller, Get, Post } from '@nestjs/common';
+import { HeroService } from './hero.service';
+import { CreateHeroDto } from './dto/create-hero.dto';
+import { Hero } from './heros.schema';
 
 @Controller('v0/hero')
 export class HeroController {
+	constructor(
+        private readonly heroService: HeroService
+	){}
+
 	@Post()
-	create(): string {
+	async create(): Promise<string> {
 		console.log("HeroController > create > creating a hero");
+
+		const anHero = await this.heroService.create({name:"geralt"});
 		return 'This action adds a new hero';
 	}
 
 	@Get()
-	findAll(): string {
+	async findAll(): Promise<Hero[]> {
 		console.log("HeroController > findAll > get all heroes");
-		return 'This action returns all heroes';
+		return await this.heroService.findAll();
 	}
 }
