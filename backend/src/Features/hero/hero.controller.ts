e08c14f318cffd70ac84ce3c96d8b687921faf3c
MarcOreliOoo <albeaurelien@gmail.com>
2023-09-07 14:09:38 +0200

    init api

--- backend/src/Features/hero/hero.controller.ts
+++ backend/src/Features/hero/hero.controller.ts
@@ -1,2 +1,16 @@
+import { Controller, Get, Post } from '@nestjs/common';
+
+@Controller('v0/hero')
 export class HeroController {
+	@Post()
+	create(): string {
+		console.log("HeroController > create > creating a hero");
+		return 'This action adds a new hero';
+	}
+
+	@Get()
+	findAll(): string {
+		console.log("HeroController > findAll > get all heroes");
+		return 'This action returns all heroes';
+	}
 }
--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,10 +3,12 @@ import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
+import { HeroModule } from "./Features/hero/hero.module";
 
 
 @Module({
-    imports: [ConfigModule.forRoot({ isGlobal: true}), MongooseModule.forRoot(process.env.MONGODB_URL)],
+    imports: [ConfigModule.forRoot({ isGlobal: true}), MongooseModule.forRoot(process.env.MONGODB_URL),
+	HeroModule],
     controllers: [AppController],
     providers: [AppService],
 })
