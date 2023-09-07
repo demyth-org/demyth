2b65c5a28f27e435b95d567a87e4b7194b1dd246
MarcOreliOoo <albeaurelien@gmail.com>
2023-09-07 11:32:29 +0200

    init create hero dto + service

--- backend/src/Features/hero/hero.service.ts
+++ backend/src/Features/hero/hero.service.ts
@@ -1,3 +1,19 @@
+import { Model } from 'mongoose';
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Hero } from './heros.schema';
+import { CreateHeroDto } from './dto/create-hero.dto';
+
+@Injectable()
 export class HeroService {
-	
+	constructor(@InjectModel(Hero.name) private heroModel: Model<Hero>) {}
+
+	async create(createHeroDto: CreateHeroDto): Promise<Hero> {
+		const createdHero = new this.heroModel(createHeroDto);
+		return createdHero.save();
+	}
+
+	async findAll(): Promise<Hero[]> {
+		return this.heroModel.find().exec();
+	}
 }
