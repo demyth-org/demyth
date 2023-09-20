8fd4510b6759a4fc6942872919049105f0842902
MarcOreliOoo <albeaurelien@gmail.com>
2023-09-20 19:54:06 +0200

    init player schema and user type

--- backend/src/Features/player/players.schema.ts
+++ backend/src/Features/player/players.schema.ts
@@ -1,13 +1,22 @@
-
-import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
-import { HydratedDocument } from 'mongoose';
+import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
+import { HydratedDocument } from "mongoose";
+import { userType } from "./enum";
 
 export type PlayerDocument = HydratedDocument<Player>;
 
-@Schema()
+@Schema({ timestamps: true })
 export class Player {
-	@Prop()
-  	name: string;
+    @Prop({ unique: true })
+    email: string;
+
+    @Prop()
+    password: string;
+
+    @Prop({ unique: true })
+    address: string;
+
+    @Prop({ required: true, type: String, enum: userType, unique: true })
+    userType: string;
 }
 
 export const PlayerSchema = SchemaFactory.createForClass(Player);
