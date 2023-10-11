ad274fa2984c18daca80c468a76b83a343c322c4
MarcOreliOoo <albeaurelien@gmail.com>
2023-10-12 00:03:25 +0200

    add motion + clsx

--- frontend/src/app/about/page.tsx
+++ frontend/src/app/about/page.tsx
@@ -3,11 +3,10 @@ import React from "react";
 import { AboutHero, AboutFeatures, AboutCodex, AboutContact, AboutCTA } from "../../components/about";
 import SectionVerticalDivider from "../../components/SectionVerticalDivider";
 
-//TODO add a ss style to the config
 const About = () => {
     return (
-        <section className="relative flex flex-col items-center justify-center gap-y-24 md:gap-y-24 w-full ">
-            <div className="relative flex flex-col items-center justify-center gap-y-8 md:gap-y-12 md:tall:justify-evenly md:tall:h-[90vh] w-full">
+        <section className="relative flex w-full flex-col items-center justify-center gap-y-24 md:gap-y-24 ">
+            <div className="relative flex w-full flex-col items-center justify-center gap-y-8 md:gap-y-12 md:tall:h-[90vh] md:tall:justify-evenly">
                 <AboutHero />
                 <AboutCTA />
                 <SectionVerticalDivider />
--- frontend/src/components/header/ToggleMenu.tsx
+++ frontend/src/components/header/ToggleMenu.tsx
@@ -18,30 +18,30 @@ const ToggleMenu = () => {
             <div
                 className={`${
                     toggle ? "flex" : "hidden"
-                } absolute top-20 right-0 mx-4 my-2 min-w-[140px] w-[140px] rounded-xl bg-black-gradient opacity-[97%]`}
+                } bg-black-gradient absolute right-0 top-20 mx-4 my-2 w-[140px] min-w-[140px] rounded-xl opacity-[97%]`}
             >
                 <ul
-                    className="flex flex-col text-end pt-2 md:pb-2 gap-y-2 w-full text-xl leading-normal"
+                    className="flex w-full flex-col gap-y-2 pt-2 text-end text-xl leading-normal md:pb-2"
                     onClick={() => setToggle((prev) => !prev)}
                 >
                     {links.map((link, index) => (
                         <li
                             key={link.hash}
-                            className={`w-full pr-4 hover:text-astral transition duration-150 ease-in-out`}
+                            className={`w-full pr-4 transition duration-150 ease-in-out hover:text-astral`}
                         >
                             <Link href={link.hash}>{link.name}</Link>
                         </li>
                     ))}
-                    <li className="flex-1 w-full md:hidden">
+                    <li className="w-full flex-1 md:hidden">
                         <button
                             type="button"
-                            className="w-full text-end pr-4 py-2 border-t border-gold-600 text-gold-600 transition duration-150 ease-in-out hover:border-astral hover:text-astral focus:bg-shark-900 focus:outline-none focus:ring-0 active:bg-shark-800 active:text-astral"
+                            className="w-full border-t border-gold-600 py-2 pr-4 text-end text-gold-600 transition duration-150 ease-in-out hover:border-astral hover:text-astral focus:bg-shark-900 focus:outline-none focus:ring-0 active:bg-shark-800 active:text-astral"
                         >
                             <Link href={"#features"}>Login</Link>
                         </button>
                         <button
                             type="button"
-                            className="w-full text-end py-2 pr-4 rounded-b-xl border border-shark-900 text-shark-900 bg-gradient-to-r from-gold-400 to-gold-600 transition duration-150 ease-in-out hover:bg-gradient-to-r hover:from-astral-300 hover:to-astral-500 focus:border-astral-600 focus:bg-gradient-to-r focus:from-astral focus:to-astral-600 focus: focus:outline-none focus:ring-0 active:border-astral active:bg-astral"
+                            className="focus: w-full rounded-b-xl border border-shark-900 bg-gradient-to-r from-gold-400 to-gold-600 py-2 pr-4 text-end text-shark-900 transition duration-150 ease-in-out hover:bg-gradient-to-r hover:from-astral-300 hover:to-astral-500 focus:border-astral-600 focus:bg-gradient-to-r focus:from-astral focus:to-astral-600 focus:outline-none focus:ring-0 active:border-astral active:bg-astral"
                         >
                             <Link href={"/about"}>Sign up</Link>
                         </button>
