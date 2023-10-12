d73d4656bafe1a1b765f42a2f08ee59c9eeafc19
MarcOreliOoo <albeaurelien@gmail.com>
2023-10-12 14:29:40 +0200

    fix motion for header

--- frontend/src/components/header/Header.tsx
+++ frontend/src/components/header/Header.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { clsx } from "clsx";
+import { motion } from "framer-motion";
 import Link from "next/link";
 import { useState } from "react";
 import { links } from "../../lib/AboutHeaderMenu";
@@ -29,15 +30,24 @@ const Header = () => {
                 <div className="hidden w-full items-center justify-end gap-4 md:flex">
                     <ul className="mr-4 hidden gap-4 lg:flex">
                         {links.map((link) => (
-                            <li key={link.hash} className="flex items-center justify-center">
+                            <li key={link.hash} className="relative flex items-center justify-center">
                                 <Link
                                     href={link.hash}
                                     onClick={() => setActiveSection(link.name)}
-                                    className={clsx("flex flex-col text-lg leading-normal hover:text-astral", {
-                                        "border-b border-astral pt-[1px] text-astral": activeSection === link.name,
-                                    })}
+                                    className={clsx("flex flex-col text-lg leading-normal hover:text-astral")}
                                 >
                                     {link.name}
+                                    {link.name === activeSection && (
+                                        <motion.span
+                                            className="absolute inset-0 -z-10 border-b border-astral pt-[1px] text-astral"
+                                            layoutId="activeSection"
+                                            transition={{
+                                                type: "spring",
+                                                stiffness: 380,
+                                                damping: 30,
+                                            }}
+                                        ></motion.span>
+                                    )}
                                 </Link>
                             </li>
                         ))}
