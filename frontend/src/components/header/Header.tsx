982b83e29f8576761f8788d1c12ccf86a8c641ea
MarcOreliOoo <albeaurelien@gmail.com>
2023-10-12 11:16:45 +0200

    header bug

--- frontend/src/components/header/Header.tsx
+++ frontend/src/components/header/Header.tsx
@@ -1,44 +1,59 @@
+"use client";
+
+import { clsx } from "clsx";
+import { motion } from "framer-motion";
 import Link from "next/link";
-import React from "react";
-import Demyth from "./Demyth";
+import { useState } from "react";
 import { links } from "../../lib/AboutHeaderMenu";
+import Demyth from "./Demyth";
 import ToggleMenu from "./ToggleMenu";
 
-//TODO add a CTA
-//TODO add login in ToggleMenu
 /*flex-1 = 
 	flex-grow=1 =>  default=0 (else allow the children to take the extra overspace of the parent containing the flex => fit the parent width container but can still overflow the container even with shrink=1 because there is a min intrinsic size of children)
 	flex-shrink=1 => default=1 (else i.e. 0, when reducing the size of the pareint containing the flex, children will overflow and flex is hence useless)
 	flex basis = 0 = width but with 0 no fixed px*/
+
 const Header = () => {
+    const [activeSection, setActiveSection] = useState("About");
+    console.log(activeSection);
     return (
-        <header className="z-[999] relative">
-            <nav className="fixed flex flex-row flex-nowrap items-center justify-between w-full p-4 md:px-8 border-b border-solid border-shark-800/70 bg-shark max-w-10xl top-0 left-1/2 -translate-x-1/2 bg-opacity-95 gap-x-6">
+        <header className="relative z-[999]">
+            <nav
+                className={clsx(
+                    "fixed left-1/2 top-0 flex w-full max-w-10xl -translate-x-1/2 flex-row flex-nowrap items-center justify-between gap-x-6 p-4",
+                    "border-b border-solid border-shark-800/70 bg-shark bg-opacity-95",
+                    "md:px-8",
+                )}
+            >
                 <Demyth />
 
-                <div className="hidden md:flex justify-end items-center gap-4 w-full">
-                    <ul className="hidden lg:flex gap-4 mr-4">
+                <div className="hidden w-full items-center justify-end gap-4 md:flex">
+                    <ul className="mr-4 hidden gap-4 lg:flex">
                         {links.map((link) => (
-                            <li
-                                key={link.hash}
-                                className="flex items-center justify-center leading-normal text-lg hover:text-astral transition duration-150 ease-in-out "
-                            >
-                                <Link href={link.hash}>{link.name}</Link>
+                            <li key={link.hash} className="relative flex items-center justify-center">
+                                <Link
+                                    href={link.hash}
+                                    onClick={() => setActiveSection(link.name)}
+                                    className={clsx(
+                                        "z-1 text-lg leading-normal transition duration-150 ease-in-out hover:text-astral",
+                                    )}
+                                >
+                                    <motion.span
+                                        layoutId="activeSection"
+                                        transition={{ type: "spring", stiffness: 100, damping: 10 }}
+                                        className={clsx("opacity-100", {
+                                            "border-b border-astral pt-[1px] text-astral": activeSection === link.name,
+                                        })}
+                                    >
+                                        {link.name}
+                                    </motion.span>
+                                </Link>
                             </li>
                         ))}
                     </ul>
-                    <button
-                        type="button"
-                        className="hidden sm:flex py-2 text-lg w-[9rem] text-center justify-center font-semibold leading-normal rounded-lg border border-gold-600 text-gold-600 transition duration-150 ease-in-out hover:border-astral hover:text-astral focus:bg-shark-900 focus:outline-none focus:ring-0 active:bg-shark-800 active:text-astral"
-                    >
-                        <Link href={"#features"}>Login</Link>
-                    </button>
-                    <button
-                        type="button"
-                        className="hidden sm:flex py-2 text-lg w-[9rem] text-center justify-center font-semibold leading-normal rounded-lg border border-shark-900 text-shark-900 bg-gradient-to-r from-gold-400 to-gold-600 transition duration-150 ease-in-out hover:bg-gradient-to-r hover:from-astral-300 hover:to-astral-500 focus:border-astral-600 focus:bg-gradient-to-r focus:from-astral focus:to-astral-600 focus: focus:outline-none focus:ring-0 active:border-astral active:bg-astral"
-                    >
-                        <Link href={"/about"}>Sign up</Link>
-                    </button>
+
+                    <ButtonBorder href="#features" label="Login" />
+                    <ButtonBg href="/about" label="Sign Up" />
                 </div>
 
                 <div className="block lg:hidden">
@@ -46,6 +61,43 @@ const Header = () => {
                 </div>
             </nav>
         </header>
+    );
+};
+
+const ButtonBorder = ({ href, label }: { href: string; label: string }) => {
+    return (
+        <button
+            type="button"
+            className={clsx(
+                "hidden w-[9rem] justify-center rounded-lg border border-gold-600 py-2 sm:flex",
+                "text-center text-lg font-semibold leading-normal text-gold-600",
+                "transition duration-150 ease-in-out",
+                "hover:border-astral hover:text-astral",
+                "focus:bg-shark-900 focus:outline-none focus:ring-0",
+                "active:bg-shark-800 active:text-astral",
+            )}
+        >
+            <Link href={href}>{label}</Link>
+        </button>
+    );
+};
+
+const ButtonBg = ({ href, label }: { href: string; label: string }) => {
+    return (
+        <button
+            type="button"
+            className={clsx(
+                "hidden w-[9rem] justify-center rounded-lg border border-shark-900 py-2",
+                "bg-gradient-to-r from-gold-400 to-gold-600",
+                "text-center text-lg font-semibold leading-normal text-shark-900",
+                "transition duration-150 ease-in-out",
+                "hover:bg-gradient-to-r hover:from-astral-300 hover:to-astral-500",
+                "focus:border-astral-600 focus:bg-gradient-to-r focus:from-astral focus:to-astral-600 focus:outline-none focus:ring-0",
+                "active:border-astral active:bg-astral sm:flex",
+            )}
+        >
+            <Link href={href}>{label}</Link>
+        </button>
     );
 };
 
